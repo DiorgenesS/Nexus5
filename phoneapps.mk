3d5f6aa72ab92d3bcf1bbd9b28996be040ab5b58
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-09 17:29:59 -0300

    initial release

---
+++ phoneapps.mk
@@ -0,0 +1,37 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothMidiService \
+	CertInstaller \
+	FaceLock \
+	KeyChain \
+	LatinImeGoogle \
+	NfcNci \
+	PacProcessor \
+	PartnerBookmarksProvider \
+	PrebuiltExchange3Google \
+	PrintSpooler \
+	qcrilmsgtunnel \
+	shutdownlistener \
+	Stk \
+	talkback \
+	TimeService \
+	UserDictionaryProvider \
+	WebViewGoogle 
+
+private-phone-priv-apps := \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	MusicFX \
+	ProxyHandler \
+	SharedStorageBackup \
+	Shell \
+	StatementService \
+	TagGoogle
