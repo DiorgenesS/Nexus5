792f1608517940d59fdf3ebde12558aa86bc59bd
Diorgenes <dyogenes_gd@hotmail.com>
2017-04-03 12:32:54 -0300

    Added files for Settings

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -54,3 +54,7 @@ if [ $1 = "SecurityCenter" ];then
     applyPatch $1 $2
 	sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
 fi
+
+if [ $1 = "Settings" ];then
+    applyPatch $1 $2
+fi
