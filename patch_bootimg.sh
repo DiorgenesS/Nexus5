459ae5ea362d053ec306b17b11541d887d30f760
Diorgenes <dyogenes_gd@hotmail.com>
2017-04-05 21:32:47 -0300

    Added information

--- patch_bootimg.sh
+++ patch_bootimg.sh
@@ -2,24 +2,33 @@
 
 BOOTIMG=$1
 
+# Unpack bootimg
 rm -rf $TARGET_BOOT_DIR
 mkdir -p $TARGET_BOOT_DIR
 $UNPACKBOOTIMG -i $BOOTIMG -o $TARGET_BOOT_DIR > /dev/null
+
+# Unpack ramdisk
 gunzip $TARGET_BOOT_DIR/boot.img-ramdisk.gz
 mkdir -p $TARGET_BOOT_DIR/ramdisk
 cd $TARGET_BOOT_DIR/ramdisk
 cpio -i < ../boot.img-ramdisk
 cd - > /dev/null
+
+# Change init
 if [ ! -f $TARGET_BOOT_DIR/ramdisk/init_vendor ];then
 mv $TARGET_BOOT_DIR/ramdisk/init $TARGET_BOOT_DIR/ramdisk/init_vendor
 fi
 cp -f $PREBUILT_BOOT_DIR/$TARGET_BIT/init $TARGET_BOOT_DIR/ramdisk/init
+
+# ramdisk miui
 if [ -d overlay/boot/ramdisk ];then
 cp -rf overlay/boot/ramdisk/* $TARGET_BOOT_DIR/ramdisk/
 fi
+
+# repack ramdisk
 $MKBOOTFS $TARGET_BOOT_DIR/ramdisk | gzip > $TARGET_BOOT_DIR/ramdisk.gz
 
-
+# Disable selinux
 OLDCMDLINE=$(cat $TARGET_BOOT_DIR/boot.img-cmdline)
 NEWCMDLINE="androidboot.selinux=permissive"
 for prop in $OLDCMDLINE
@@ -38,4 +47,5 @@ PAGESIZE=$(cat $TARGET_BOOT_DIR/boot.img-pagesize)
 RAMDISKOFFSET=$(cat $TARGET_BOOT_DIR/boot.img-ramdisk_offset)
 TAGSOFFSET=$(cat $TARGET_BOOT_DIR/boot.img-tags_offset)
 
+# repack bootimg
 $MKBOOTIMG --kernel $TARGET_BOOT_DIR/boot.img-zImage --ramdisk $TARGET_BOOT_DIR/ramdisk.gz --dt $TARGET_BOOT_DIR/boot.img-dt --base "$BASEADDR" --pagesize "$PAGESIZE" --ramdisk_offset "$RAMDISKOFFSET" --tags_offset "$TAGSOFFSET" --cmdline "$NEWCMDLINE" -o $BOOTIMG
