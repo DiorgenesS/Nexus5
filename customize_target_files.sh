5d60bb14dea9ba196ba1a11293835ee9cbad2ceb
Diorgenes <dyogenes_gd@hotmail.com>
2017-04-29 23:27:18 -0300

    moved file other/hammerhead.xml to miuisystem.apk/assets to prevent bugs Signed-off-by: Diorgenes <dyogenes_gd@hotmail.com>

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -9,9 +9,6 @@ OTHER_DIR=$PWD/other
 cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
 
-#Added device features
-cp -f other/hammerhead.xml out/target_files/SYSTEM/etc/device_features
-
 #Added multi cust variants for miui
 rm -rf out/target_files/DATA/miui/cust
 cp -rf other/miui out/target_files/DATA/
