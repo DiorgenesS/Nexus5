ae1f86483e8ffe85d692f2f9e4cefb668dadeadc
Diorgenes <dyogenes_gd@hotmail.com>
2017-05-03 01:39:32 -0300

    details :/

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -48,7 +48,7 @@ fi
 
 if [ $1 = "SecurityCenter" ];then
     applyPatch $1 $2
-	sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
+    sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
 fi
 
 if [ $1 = "TeleService" ];then
@@ -57,5 +57,5 @@ fi
 
 if [ $1 = "XiaomiAccount" ];then
     applyPatch $1 $2
-	sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
+    sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
 fi
