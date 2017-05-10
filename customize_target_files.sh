bdeb7612e253a9de37a02182508e4c98733cf4a2
Diorgenes <dyogenes_gd@hotmail.com>
2017-05-10 04:10:36 -0300

    Use custom updater bin file Signed-off-by: Diorgenes <dyogenes_gd@hotmail.com>

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -6,6 +6,13 @@ TARGET_FILES_DIR=$OUT_DIR/target_files
 build_prop_file=$TARGET_FILES_DIR/SYSTEM/build.prop
 OTHER_DIR=$PWD/other
 
+#Use custom updater bin file
+if [ -e $METADATA_DIR/updater ]
+then
+   echo "Use custom updater bin file"
+   cp $METADATA_DIR/updater $TARGET_FILES_DIR/OTA/bin
+fi
+
 cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
 
